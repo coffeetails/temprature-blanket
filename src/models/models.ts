38744631cbe84076490b58fd06f33b2fbf5b2785
Monica Björk <m.bjork95@gmail.com>
2022-10-23 13:47:02 +0200

    added models

---
+++ src/models/models.ts
@@ -0,0 +1,16 @@
+export type Weather = {
+  location: string;
+  unit: string;
+  useMyApi: boolean;
+  days: Day[];
+}
+
+export type Day = {
+  datetime: string;
+  hours: Hours[];
+}
+
+export type Hours = {
+  temp: number;
+  datetime: string;
+}
