399aff706cb1bbadfbca6d895a542fbe61d52a77
Monica Björk <m.bjork95@gmail.com>
2022-11-27 16:52:48 +0100

    added more info about visualcrossing

--- src/Views/SearchView.tsx
+++ src/Views/SearchView.tsx
@@ -17,12 +17,11 @@ function SearchView({setDisplayWeather}: Props) {
 				<p>Gör en sökning nedan för att få fram tidigare temperaturer på din valda plats. Om du vill hämta mycket väderdata så kan du skapa din egna nyckel hos <a href="https://www.visualcrossing.com/weather/weather-data-services">visualcrossing.com</a></p>
 
 				<details>
-					<summary>Läs mer</summary>
-					<p>https://www.visualcrossing.com/</p>
-					<p>Visual Crossing Weather is the easiest-to-use and lowest-cost source for historical and forecast weather data. Our weather API is designed to integrate easily into any app or code, and our prices are lower than any other provider in the industry.</p>
+					<summary>Läs mer här.. </summary>
+					<p><a href="https://www.visualcrossing.com/weather/weather-data-services">visualcrossing.com</a></p>
+					<p>"Visual Crossing Weather is the easiest-to-use and lowest-cost source for historical and forecast weather data. Our weather API is designed to integrate easily into any app or code, and our prices are lower than any other provider in the industry."</p>
+					<p>Du skapar en inloggning på visualcrossing och på sidan med kontoinformation så kan du hitta din "{"</>"} key", denna nycken kan du skriva i nedan i fältet för "API Nyckel". Som sagt så är detta inte något du behöver.</p>
 				</details>
-
-				<br /> {/* booo! I'm lazy */}
 
 				<Form setDisplayWeather={setDisplayWeather} />
 			</article>
