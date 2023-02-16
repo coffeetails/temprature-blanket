85fefbcdbcce9b072ed0298dc46f6f187500c34c
Monica Björk <m.bjork95@gmail.com>
2023-02-16 09:00:25 +0100

    Tested supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-
+import { createClient } from '@supabase/supabase-js'
 import HomeView from './Views/HomeView';
 import AboutView from './Views/AboutView';
 import ColorView from './Views/ColorView';
@@ -10,7 +10,15 @@ import { Weather } from './models/models';
 
 function App() {
     const [displayWeather, setDisplayWeather] = useState<Weather | null>(null);
+    
+    const supabase = createClient('https://pxpnvjvtmnvoarrcxtse.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4cG52anZ0bW52b2FycmN4dHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0ODgxNzksImV4cCI6MTk5MjA2NDE3OX0.gdRuDktzgUFsO7QSqO9a0spUeJDBhpXUMqVc7zvFzPQ')
 
+    async function getCountries() {
+      const countries = await supabase.from('countries').select()
+      console.log(countries)
+    }
+    
+    getCountries();
     return (
         <Routes>
             <Route path="/" element={ <HomeView displayWeather={displayWeather} setDisplayWeather={setDisplayWeather} /> } />
