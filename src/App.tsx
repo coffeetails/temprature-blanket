0717d8e3b86793b73e1931c0bb7af6ce4ff2dfd4
Monica Björk <m.bjork95@gmail.com>
2023-02-17 10:23:50 +0100

    get data from supabase works

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,8 @@ function App() {
     const supabase = createClient('https://pxpnvjvtmnvoarrcxtse.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4cG52anZ0bW52b2FycmN4dHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0ODgxNzksImV4cCI6MTk5MjA2NDE3OX0.gdRuDktzgUFsO7QSqO9a0spUeJDBhpXUMqVc7zvFzPQ')
 
     async function getData() {
-        const countries = await supabase.from('countries').select()
-        console.log(countries.data)
         const temperatures = await supabase.from('temperatures').select();
-        console.log(temperatures.data)
-        const data = await supabase.from('temperatures').select('*')
-        console.log(JSON.stringify(data))
+        console.log("temperatures", temperatures)
     }
     
     getData();
