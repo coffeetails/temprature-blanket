c33cd898852fb360a0d5060bba6686a01b30c214
Monica Björk <m.bjork95@gmail.com>
2023-06-30 20:19:11 +0200

    import redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { createStore } from 'redux'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
